740b8b0615b7c7765e9037bfa463459f15f08026
Ajithesh9 <ajithesh1418@gmail.com>
2025-10-09 12:00:49 +0530

    Implement detailed error handling in errorHandler middleware and add constants for error types

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,6 +1,38 @@
+const {constants} = require("../constants")
 const errorHandler = (err,req,res,next) =>{
     const statusCode = res.statusCode ? res.statusCode :500;
-    res.json({message: err.message, statusTrace: err.stack  })
+    switch (statusCode) {
+        case constants.VALIDATION_ERROR:
+            res.json({ 
+                title: "Validation Failed" ,
+                message: err.message, 
+                statusTrace: err.stack  
+            });
+            break;
+            case constants.NOT_FOUND:
+                res.json({ title: "Not Found" ,
+                    message: err.message, 
+                    statusTrace: err.stack  
+                });
+            case constants.UNAUTHORIZED:
+                res.json({ title: "UNAUTHORIZED" ,
+                    message: err.message, 
+                    statusTrace: err.stack  
+                });
+            case constants.FORBIDDEN:
+                res.json({ title: "FORBIDDEN" ,
+                    message: err.message, 
+                    statusTrace: err.stack  
+                });
+            case constants.SERVER_ERROR:
+                res.json({ title: "SERVER_ERROR" ,
+                    message: err.message, 
+                    statusTrace: err.stack  
+                });
+            default:
+                console.log("No error, All Good!")
+                break;
+    }
 };
 
 module.exports = errorHandler;
